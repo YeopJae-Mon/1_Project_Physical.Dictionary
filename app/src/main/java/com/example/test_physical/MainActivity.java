80c562a8bc90bdb76da9682703ddc1af71386baf
YeopJae-Mon <1015samuel@naver.com>
2021-04-23 03:04:42 +0900

    2021-04-23 Test DB Fail first

--- app/src/main/java/com/example/test_physical/MainActivity.java
+++ app/src/main/java/com/example/test_physical/MainActivity.java
@@ -7,52 +7,51 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
-    DBHelper helper;
+    dbHelper helper;
     SQLiteDatabase db;
+    EditText edit_name, edit_tel;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ListView listView =(ListView)findViewById(R.id.listview);
-
-        helper = new DBHelper(MainActivity.this, "test.db", null, 1);
+        helper = new dbHelper(this);
         db = helper.getWritableDatabase();
-        helper.onCreate(db);
-
-        String sql = "select * from mytable;";
-        Cursor c = db.rawQuery(sql, null);
-        String[] strs = new String[]{"txt"};
-        int[] ints = new int[] {R.id.listview_txt};
-
-        SimpleCursorAdapter adapter = null;
-        adapter = new SimpleCursorAdapter(listView.getContext(), R.layout.listview, c, strs, ints,0);
-
-        listView.setAdapter(adapter);
+        Cursor cursor = db.rawQuery("SELECT * FROM contacts", null);
+        startManagingCursor(cursor);
+        String[] from = { "name", "tel" };
+        int[] to = { android.R.id.text1, android.R.id.text2 };
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor, from, to);
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setAdapter(adapter);
 
     }
+}
 
-    public class DBHelper extends SQLiteOpenHelper {
-        public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-            super(context, name, factory, version);
+class dbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "test.db";
+    private static final int DATABASE_VERSION = 2;
+
+    public dbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE contacts ( _id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "name TEXT, tel TEXT);");
+        for (int i = 0; i < 10; i++) {
+            db.execSQL("INSERT INTO contacts VALUES (null, " + "'김철수 " + i
+                    + "'" + ", '010-1234-100" + i + "');");
         }
+    }
 
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            String sql = "DROP TABLE if exists mytable";
-
-            db.execSQL(sql);
-            onCreate(db);
-        }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS contacts");
+        onCreate(db);
     }
 }
