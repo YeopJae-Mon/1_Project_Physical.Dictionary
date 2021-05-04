ae4a3c9430218874d9028d37f9922c14af078a58
YeopJae-Mon <1015samuel@naver.com>
2021-05-04 23:45:57 +0900

    2021-05-04 Test Example Third

--- app/src/main/java/com/example/test_physical/MemoRecord.java
+++ app/src/main/java/com/example/test_physical/MemoRecord.java
@@ -1,4 +1,15 @@
 package com.example.test_physical;
 
 public class MemoRecord {
+    public Integer id;	 // id
+    public String title; // 제목
+    public String text; // 텍스트
+    public Long timeStamp; // 타임스탬프
+
+    public MemoRecord(Integer id, String title, String text, Long timestamp) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.timeStamp = timestamp;
+    }
 }
